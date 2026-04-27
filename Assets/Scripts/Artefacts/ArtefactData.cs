95db75e9ad705decd2365e8098c84720990e69b6
Denis Baran <dendombar221@gmail.com>
2026-04-27 21:44:11 +0300

    feat: artefact system set up

---
+++ Assets/Scripts/Artefacts/ArtefactData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New_Artefact", menuName = "Scriptable Objects/Artefact")]
+public class ArtefactData : ScriptableObject
+{
+    public Sprite icon;
+    [TextArea]
+    public string description;
+
+    public enum Type
+    {
+        CoinMultiplier,      // Умножает всё золото
+        UpgradeDiscount,     // Скидка на покупку
+        PrestigeBonus,       // Бонус к валюте перерождения
+        ChanceToDouble       // Шансы выпадения норм предметов увеличиваются
+    }
+    public Type type;
+}
